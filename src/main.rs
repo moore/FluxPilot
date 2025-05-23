489547d1159751acd5ad47bc5d0da3a7c21f4373
Jonathan Moore <moore@eds.org>
2025-05-22 21:39:17 -0700

    lights and serial over usb working in tandum

--- src/main.rs
+++ src/main.rs
@@ -10,56 +10,130 @@ use hal::gpio::{AnyPin, Level, Output, Pin};
 use hal::prelude::*;
 use hal::spi::Spi;
 use hal::{peripherals, println};
+
+use embassy_executor::Spawner;
+use embassy_futures::join::join3;
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::driver::EndpointError;
+use embassy_usb::Builder;
+use embassy_time::{Duration, Timer};
+
+use ch32_hal::usart;
+use ch32_hal::usbd::Driver;
+use ch32_hal::usbd::Instance;
+use ch32_hal::usbd::InterruptHandler;
+use ch32_hal::usb::EndpointDataBuffer;
+use ch32_hal::bind_interrupts;
+
 use {ch32_hal as hal, panic_halt as _};
+
 use smart_leds::{brightness, SmartLedsWrite, RGB8};
 use ws2812_spi as ws2812;
 use crate::ws2812::Ws2812;
 
 
-#[qingke_rt::entry]
-fn main() -> ! {
+
+
+bind_interrupts!(struct Irqs {
+    USB_LP_CAN1_RX0 => ch32_hal::usbd::InterruptHandler<peripherals::USBD>;
+});
+
+#[embassy_executor::main(entry = "qingke_rt::entry")]
+async fn main(_spawner: Spawner) {
     hal::debug::SDIPrint::enable();
     let mut config = hal::Config::default();
     config.rcc = hal::rcc::Config::SYSCLK_FREQ_144MHZ_HSI;
     let p = hal::init(config);
 
-    // SPI1, remap 0
-    let sck = p.PA5;
+    let usb: peripherals::USBD = p.USBD;
+
+    /* USB DRIVER SECION */
+    let driver = Driver::new(usb, Irqs, p.PA12, p.PA11);
+  
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xC0DE, 0xCAFE);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    // Windows compatibility requires these; CDC-ACM
+    config.device_class = 0x02;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x00;
+    config.composite_with_iads = false;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    //let mut msos_descriptor = [0; 256];
+    let mut msos_descriptor = [0; 0];
+    let mut control_buf = [0; 64];
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
+
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    // Do stuff with the class!
+    let echo_fut = async {
+        loop {
+            class.wait_connection().await;
+            //println!("Connected1");
+            let _ = echo(&mut class).await;
+            //println!("Disconnected1");
+        }
+    };
+
+    ////////////////////////////////////////////
+
+    // SPI1
     let MOSI = p.PA7;
-
-    let rst = p.PB0;
-
-
-    let mut rst = Output::new(rst, Level::High, Default::default());
-
 
     let mut spi_config = hal::spi::Config::default();
     spi_config.frequency = Hertz::mhz(3);
     spi_config.mode = ws2812::MODE;
 
-    let spi = Spi::new_blocking_txonly(p.SPI1, sck, MOSI, spi_config);
-
-    rst.set_low();
-    riscv::asm::delay(120_000_000);
-    rst.set_high();
-    riscv::asm::delay(20_000_000);
+    let spi = Spi::new_blocking_txonly_nosck(p.SPI1, MOSI, spi_config);
 
     let mut ws = Ws2812::new(spi);
-
-   
 
     const NUM_LEDS: usize = 25;
     let mut data = [RGB8::default(); NUM_LEDS];
 
-    loop {
-        for j in 0..(256 * 5) {
-            for i in 0..NUM_LEDS {
-                data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
+    let led_loop = async {
+        loop {
+            for j in 0..(256 * 5) {
+                for i in 0..NUM_LEDS {
+                    data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
+                }
+                ws.write(brightness(data.iter().cloned(), 32)).unwrap();
+                Timer::after(Duration::from_millis(10)).await;
+
             }
-            ws.write(brightness(data.iter().cloned(), 32)).unwrap();
-            riscv::asm::delay(500_000);
         }
-    }
+    };
+
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join3(usb_fut, echo_fut, led_loop).await;
 }
 
 /// Input a value 0 to 255 to get a color value
@@ -75,4 +149,24 @@ fn wheel(mut wheel_pos: u8) -> RGB8 {
     }
     wheel_pos -= 170;
     (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        class.write_packet(data).await?;
+    }
 }
